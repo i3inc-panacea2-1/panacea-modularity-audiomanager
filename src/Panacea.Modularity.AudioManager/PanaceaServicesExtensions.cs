8f3dbd8f5a5db8a20fa4516dcf527dce26b6dfb7
Portno <j.remoundos@dotbydot.gr>
2019-06-24 16:11:30 +0300

    added extensions

---
+++ src/Panacea.Modularity.AudioManager/PanaceaServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Panacea.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.AudioManager
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static bool TryGetAudioManager(this PanaceaServices core, out IAudioManager audio)
+        {
+            audio = core.PluginLoader.GetPlugins<IAudioManagerPlugin>()?.FirstOrDefault()?.GetAudioManager();
+            return audio != null;
+        }
+    }
+}
