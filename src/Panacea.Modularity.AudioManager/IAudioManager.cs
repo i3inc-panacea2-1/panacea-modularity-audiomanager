da0cafd3ae0e08a955040540c9e9c359c2b0be5f
Portno <j.remoundos@dotbydot.gr>
2019-06-24 15:37:40 +0300

    first commit

---
+++ src/Panacea.Modularity.AudioManager/IAudioManager.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Panacea.Modularity.AudioManager
+{
+    public interface IAudioManager:INotifyPropertyChanged
+    {
+        int SpeakersVolume { get; set; }
+
+        int MicrophoneVolume { get; set; }
+    }
+}
